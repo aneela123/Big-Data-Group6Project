492001652eb99ec65d7d1cf47d086d083cadb5cd
aneela123 <77805472+aneela123@users.noreply.github.com>
2022-04-19 20:28:56 -0500

    Added DoFn code and its comments.

--- Aneela/src/main/java/edu/nwmissouri/BigDataGroup6/RankedPage.java
+++ Aneela/src/main/java/edu/nwmissouri/BigDataGroup6/RankedPage.java
@@ -2,5 +2,10 @@ package edu.nwmissouri.BigDataGroup6;
 import java.util.ArrayList;
 
 public class RankedPage {
+
+    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    }
+
+
     
 }
--- Aneela/src/main/java/edu/nwmissouri/BigDataGroup6/VotingPage.java
+++ Aneela/src/main/java/edu/nwmissouri/BigDataGroup6/VotingPage.java
@@ -2,5 +2,8 @@ package edu.nwmissouri.BigDataGroup6;
 import java.util.ArrayList;
 
 public class VotingPage {
+
+    public VotingPage(String voterName, Integer contributorVotes) {
+    }
     
 }
