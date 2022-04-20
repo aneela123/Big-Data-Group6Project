0c3653abf3be5bdc8de3840bd57fbab48487444d
Priyanka1818 <77811257+Priyanka1818@users.noreply.github.com>
2022-04-19 21:28:45 -0500

    Update RankedPage.java

---
+++ Priyanka/src/main/java/edu/nwmissouri/BigDataGroup6/RankedPage.java
@@ -0,0 +1,11 @@
+package edu.nwmissouri.BigDataGroup6;
+import java.util.ArrayList;
+
+public class RankedPage {
+
+    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    }
+
+
+    
+}
